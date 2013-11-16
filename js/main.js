52e49600e08bc709d36f2fec1ae4bf4cdad4b420
Vova <metal_god@ovi.com>
2013-11-17 00:10:53 +0200

    changed ships - they are generated automatically, added left right movement to gunMachine

--- js/main.js
+++ js/main.js
@@ -1,36 +1,54 @@
 window.onload = function(){
 	var field = document.getElementsByClassName("wrapper")[0],
-	Machine = function (_id, _class){
+	Machine = function (_id, _class, leftPoint, topPoint){
+		this.startLeft = leftPoint;
+		this.startTop = topPoint;
 		this.height = 30;
 		this.width = 30;
 		this._id = _id;
 		this._class = _class;
+		this.currentElement = "";
 		this.createElement = function(){
 			var domElement = document.createElement("div");
-			domElement.className = this._class + " " + this._id;
+			domElement.className = this._class + " " + "elem" + this._id;
 			domElement.style.width = this.width + "px";
 			domElement.style.height = this.height + "px";
-			console.log(field);
+			domElement.style.left = this.startLeft + "px";
+			domElement.style.top = this.startTop + "px";
 			field.appendChild(domElement);
+			this.currentElement = document.getElementsByClassName("elem" + this._id)[0];
 		}
 	},
-	i = 0,
 	elem = [];
-	Killer = function(_id, _class){
+	var Killer = function(_id, _class, leftPoint, topPoint){
 		this._id = _id;
 		this._class = _class;
+		this.startLeft = leftPoint;
+		this.startTop = topPoint;
 		this.shut = function(){
 
 		};
-		this.move = function(){
-			alert("yes");
+		this.move = function(direction){
+			var regExp = /[0-9]+/;
+			var currentLeft = (this.currentElement.style.left).match(regExp);
+			console.log(currentLeft);
+			if(direction == "left" && currentLeft != 0){
+				this.currentElement.style.left =  0 + currentLeft - 30 + "px";
+			}else if(direction == "right" && (!(currentLeft >= 210))){
+				this.currentElement.style.left =  30 + parseInt(currentLeft) + "px";
+			}else{
+				throw "Wrong direction";
+			}
+			
 		}
 	}
 	Killer.prototype = new Machine();
 
-	Ship = function(_id, _class){
+	var Ship = function(_id, _class, leftPoint, topPoint){
 		this._id = _id;
 		this._class = _class;
+		this.startLeft = leftPoint;
+		this.startTop = topPoint;
 		this.destroyed = function(){
 
 		}
@@ -40,11 +58,16 @@ window.onload = function(){
 	}
 	Ship.prototype = new Machine();
 
-	var kill =  new Killer("killer", "killer");
+	var kill =  new Killer("killer", "killer", 90, 300);
+	console.log(kill);
 	kill.createElement();
-	for(i; i < 16; i++){
-		elem[i] = new Ship(i, "ship");
-		elem[i].createElement();
+	for(var i = 0, leftPoint = 0, topPoint = 0; i < 16; i++, leftPoint += 30){
+		if(i == 8){
+			topPoint = 30;
+			leftPoint = 0;
+		}
+			elem[i] = new Ship(i, "ship", leftPoint, topPoint);
+			elem[i].createElement();
 	}
 	var CO = {
 		kill : document.getElementsByClassName("killer")[0],
@@ -58,12 +81,12 @@ window.onload = function(){
 		break;
 		case 37 : {
 			// left key
-			CO.kill.style.left = CO.kill.style.left30 + "px";
+			kill.move("left");
 		}
 		break;
 		case 39 : {
 			// right key
-			console.log("right key");
+			kill.move("right");
 		}
 		break;
 		}
