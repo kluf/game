cd087a0cbd930f5a56109a351cbffc37ef3a369a
Vova <metal_god@ovi.com>
2013-11-17 18:31:16 +0200

    added posibility for shutting, but it doesn't work correct.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 window.onload = function(){
 	var field = document.getElementsByClassName("wrapper")[0],
-	Machine = function (_id, _class, leftPoint, topPoint){
+	Machine = function (_id, _class, leftPoint, topPoint, column){
 		this.startLeft = leftPoint;
+		this.column = column;
 		this.startTop = topPoint;
 		this.height = 30;
 		this.width = 30;
@@ -20,22 +21,47 @@ window.onload = function(){
 		}
 	},
 	elem = [];
-	var Killer = function(_id, _class, leftPoint, topPoint){
+	var Killer = function(_id, _class, leftPoint, topPoint, column){
 		this._id = _id;
 		this._class = _class;
 		this.startLeft = leftPoint;
 		this.startTop = topPoint;
+		this.column = 3;
+		this.tempArr = [];
+		this.targetShip = "";
 		this.shut = function(){
-
+			try{
+				for(var i = 0; i < elem.length; i += 1){
+					console.log(elem[i].row);
+						this.tempArr.push(elem[i].getId(this.column, elem[i].row));
+						// this.tempArr.push(elem[i].row);
+						// console.log(this.tempArr)
+					
+					// console.log(this.tempArr);
+					
+				}
+				elem[this.tempArr.sort(function(a, b){
+					return b - a;
+				})[0]].destroy();
+				this.tempArr.shift();
+				console.log(this.tempArr);
+				// this.targetShip = elem[i].getId(this.column, this.tempArr[0]);
+				// console.log(this.targetShip)
+				// elem[this.column].destroy();
+			}catch(e){
+				console.log(e);
+			}
 		};
 		this.move = function(direction){
 			var regExp = /[0-9]+/;
 			var currentLeft = (this.currentElement.style.left).match(regExp);
-			console.log(currentLeft);
 			if(direction == "left" && currentLeft != 0){
 				this.currentElement.style.left =  0 + currentLeft - 30 + "px";
+				this.column -= 1;
 			}else if(direction == "right" && (!(currentLeft >= 210))){
 				this.currentElement.style.left =  30 + parseInt(currentLeft) + "px";
+				this.column += 1;
+				// console.log(this.column);
 			}else{
 				throw "Wrong direction";
 			}
@@ -44,13 +70,21 @@ window.onload = function(){
 	}
 	Killer.prototype = new Machine();
 
-	var Ship = function(_id, _class, leftPoint, topPoint){
+	var Ship = function(_id, _class, leftPoint, topPoint, column, row){
 		this._id = _id;
 		this._class = _class;
 		this.startLeft = leftPoint;
 		this.startTop = topPoint;
-		this.destroyed = function(){
-
+		this.column = column;
+		this.row = row;
+		this.destroy = function(){
+			this.currentElement.parentNode.removeChild(this.currentElement);
+			elem[this._id].shift();
+		}
+		this.getId = function(column, row){
+			if(this.column == column && this.row == row){
+				return this._id;
+			}
 		}
 		this.moveDown = function(){
 
@@ -58,25 +92,27 @@ window.onload = function(){
 	}
 	Ship.prototype = new Machine();
 
-	var kill =  new Killer("killer", "killer", 90, 300);
-	console.log(kill);
+	var kill =  new Killer("killer", "killer", 90, 300, 3);
 	kill.createElement();
-	for(var i = 0, leftPoint = 0, topPoint = 0; i < 16; i++, leftPoint += 30){
+	for(var i = 0, leftPoint = 0, topPoint = 0, elemRow = 0, elemColumn = 0; i < 16; i++, leftPoint += 30, elemColumn += 1){
 		if(i == 8){
 			topPoint = 30;
 			leftPoint = 0;
+			elemRow = 1;
+			elemColumn = 0;
 		}
-			elem[i] = new Ship(i, "ship", leftPoint, topPoint);
+			elem[i] = new Ship(i, "ship", leftPoint, topPoint, elemColumn, elemRow);
 			elem[i].createElement();
+			// console.log(elem[i]);
 	}
-	var CO = {
-		kill : document.getElementsByClassName("killer")[0],
-	}
+	// var CO = {
+	// 	kill : document.getElementsByClassName("killer")[0],
+	// }
 	document.body.addEventListener("keydown",function(event){
 		switch(event.keyCode){
 		case 32 : {
 			// spacebar
-			console.log("spacebar");
+			kill.shut();
 		}
 		break;
 		case 37 : {
